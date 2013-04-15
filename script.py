66dc848b1c1b15334c40c16f0514cf436cb30139
giridha2 <giridha2@illinois.edu>
2013-04-14 23:50:50 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 from Bio import Phylo
 tree = Phylo.read("sample.dnd", "newick")
 tree.rooted = True
-Phylo.draw(tree)
-
 ancestor = tree.common_ancestor("E","A")
 ancestor.name = "Common_E_A"
 Phylo.draw(tree)
